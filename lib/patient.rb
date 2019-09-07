2f1bc2da6de4e2909ded4e1dccd916cd767cc547
Isai Bismark Cortez <isai0505@gmail.com>
2019-09-07 04:06:18 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,29 @@
+class Patient
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment
+  end
+
+  def appointments
+  end
+
+  def doctors
+  end
+
+end
